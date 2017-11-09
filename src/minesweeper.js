38ad7b5de3ede04408391a777784e268e8563000
Greg Taylor <hesitatetowonder@gmail.com>
2017-11-09 14:41:26 -0800

    dynamically generate player board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,20 +1,24 @@
-const printBoard = (board) => {
-console.log('Current board : ');
-console.log(board[0].join(' | '));
-console.log(board[1].join(' | '));
-console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex ++) {
+    let row = [];
+    for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex ++) {
+      row.push(' ');
+    }
+      board.push(row);
+  }
+  return board;
 };
+// end of generatePlayerBoard
 
-
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
-
-
-
-printBoard(board);
-board [0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+const generateBombBoard = (numberofRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex ++) {
+    let row = [];
+    for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex ++) {
+      row.push(' ');
+    }
+      board.push(row);
+  }
+  return board;
+};
