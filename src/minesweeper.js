b8e9741440906244f6826e680110658e36a5f71e
Greg Taylor <hesitatetowonder@gmail.com>
2017-11-09 15:43:57 -0800

    generate bomb board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -11,14 +11,40 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 };
 // end of generatePlayerBoard
 
-const generateBombBoard = (numberofRows, numberOfColumns, numberOfBombs) => {
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   let board = [];
   for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex ++) {
     let row = [];
     for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex ++) {
-      row.push(' ');
+      row.push(null);
     }
       board.push(row);
   }
+let numberOfBombsPlaced = 0;
+//Currently can place bombs on top of existing bombs
+while(numberOfBombsPlaced < numberOfBombs) {
+  let randomRowIndex =
+  Math.floor((Math.random() * numberOfRows) + 1);
+
+  let randomColumnIndex =
+  Math.floor((Math.random() * numberOfColumns) + 1);
+
+  board[randomRowIndex][randomColumnIndex] = 'B';
+
+  numberOfBombsPlaced++;
+};
   return board;
 };
+
+const printBoard = (board) => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+//End of current gameplay code
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player Board : ');
+printBoard(playerBoard);
+console.log('Bomb Board : ');
+printBoard(bombBoard);
