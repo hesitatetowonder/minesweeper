69a1b821ee1a667597f973b53f01849c6141438a
Greg Taylor <hesitatetowonder@gmail.com>
2017-11-30 10:48:47 -0800

    final version minesweeper

--- src/board.js
+++ src/board.js
@@ -1,26 +1,4 @@
-class Game{
-
-  constructor(numberOfRows, numberOfColumns, numberOfBombs){
-    this._board = new Board(numberOfColumns, numberOfRows,numberOfBombs);
-  };
-
-  playMove(rowIndex, columnIndex) {
-    this._board.flipTile(rowIndex, columnIndex);
-    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log('Sorry, you blew up');
-      this._board.print();
-    } else if (!this._board.hasSafeTiles()){
-      console.log('Congratulations, you win!');
-      // print(this._board);
-    } else {
-      console.log('Current Board: ');
-      this._board.print();
-    };
-
-  };
-}
-
-class Board{
+export class Board{
 
   constructor(numberOfRows, numberOfColumns, numberOfBombs){
     this._numberOfBombs = numberOfBombs;
@@ -112,7 +90,4 @@ class Board{
     return board;
   };
   //end of generateBombBoard
-  //end board class
 };
-const g = new Game(3,3,3);
-g.playMove(0,0);
